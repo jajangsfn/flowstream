9d92e0d987ba9b0c0e6cf082305d00120afe0a74
Nathanael <nael.nathanael71@gmail.com>
2020-07-15 21:00:18 +0800

    landing responsive

--- application/views/landing/index.php
+++ application/views/landing/index.php
@@ -1,6 +1,6 @@
 <div class="d-flex justify-content-center align-items-center h-100">
     <div class="row w-75">
-        <div class="col-4 p-5 text-center">
+        <div class="col-md-4 col-lg-4 col-sm-6 p-5 text-center">
             <a href="<?= base_url("/index.php/dashboard") ?>" class="btn btn-shadow btn-icon btn-circle btn-primary display-2">
                 <i class="flaticon2-graphic display-3"></i>
             </a>
@@ -8,7 +8,7 @@
                 <?= $this->lang->line('menu_dashboard') ?>
             </h3>
         </div>
-        <div class="col-4 p-5 text-center">
+        <div class="col-md-4 col-lg-4 col-sm-6 p-5 text-center">
             <a href="<?= base_url("/index.php/penjualan/home") ?>" class="btn btn-shadow btn-icon btn-circle btn-primary display-2">
                 <i class="flaticon2-shopping-cart-1  display-3"></i>
             </a>
@@ -16,7 +16,7 @@
                 <?= $this->lang->line('menu_sales') ?>
             </h3>
         </div>
-        <div class="col-4 p-5 text-center">
+        <div class="col-md-4 col-lg-4 col-sm-6 p-5 text-center">
             <a href="<?= base_url("/index.php/pembelian") ?>" class="btn btn-shadow btn-icon btn-circle btn-primary display-2">
                 <i class="flaticon2-lorry  display-3"></i>
             </a>
@@ -24,7 +24,7 @@
                 <?= $this->lang->line('menu_purchases') ?>
             </h3>
         </div>
-        <div class="col-4 p-5 text-center">
+        <div class="col-md-4 col-lg-4 col-sm-6 p-5 text-center">
             <a href="<?= base_url("/index.php/inventori") ?>" class="btn btn-shadow btn-icon btn-circle btn-primary display-2">
                 <i class="flaticon2-open-box  display-3"></i>
             </a>
@@ -32,15 +32,15 @@
                 <?= $this->lang->line('menu_inventory') ?>
             </h3>
         </div>
-        <div class="col-4 p-5 text-center">
+        <div class="col-md-4 col-lg-4 col-sm-6 p-5 text-center">
             <a href="<?= base_url("/index.php/keuangan") ?>" class="btn btn-shadow btn-icon btn-circle btn-primary display-2">
-                <i class="flaticon-piggy-bank  display-3"></i>
+                <i class="flaticon-analytics  display-3"></i>
             </a>
             <h3 class="mt-2">
                 <?= $this->lang->line('menu_finance') ?>
             </h3>
         </div>
-        <div class="col-4 p-5 text-center">
+        <div class="col-md-4 col-lg-4 col-sm-6 p-5 text-center">
             <a href="<?= base_url("/index.php/setting") ?>" class="btn btn-shadow btn-icon btn-circle btn-primary display-2">
                 <i class="flaticon2-gear  display-3"></i>
             </a>
